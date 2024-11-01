6f75b5e42dcf8945e9fd94d0e50ba00c1a7fca77
dsheehan2 <dylansheehan493@gmail.com>
2024-10-31 19:08:50 -0500

    task/PWB-10: Adding Clickable Links, Phone Num, Email

--- personal-website-frontend/src/components/BusinessCard.vue
+++ personal-website-frontend/src/components/BusinessCard.vue
@@ -13,13 +13,68 @@
         </q-card-section>
       </q-card-section>
 
-      <!-- <q-card-actions>
-        <q-btn flat round icon="event" />
-        <q-btn flat> 7:30PM </q-btn>
-        <q-btn flat color="primary"> Reserve </q-btn>
-      </q-card-actions> -->
-
       <q-separator />
+
+      <q-card-actions>
+        <q-btn class="button-card" flat @click="copyText(email)">
+          {{ email }}
+        </q-btn>
+        <q-btn style="margin-right: 100px" flat @click="copyText(phoneNum)">
+          {{ phoneNum }}
+        </q-btn>
+        <q-btn
+          class="button-card"
+          href="https://www.linkedin.com/in/dylsheehan/"
+          flat
+          round
+          size="0.75em"
+        >
+          <img
+            style="height: 3em"
+            src="../assets/linkedin-button.png"
+            alt="LinkedIn Image"
+          />
+        </q-btn>
+        <q-btn
+          class="button-card"
+          href="https://github.com/dsheehan2"
+          flat
+          round
+          size="0.75em"
+        >
+          <img
+            style="height: 3em"
+            src="../assets/github-button.png"
+            alt="GitHub Image"
+          />
+        </q-btn>
+        <q-btn
+          class="button-card"
+          href="https://www.instagram.com/dylan_sheehann/"
+          flat
+          round
+          size="0.75em"
+        >
+          <img
+            style="height: 3em"
+            src="../assets/instagram-button.png"
+            alt="Instagram Image"
+          />
+        </q-btn>
+        <q-btn
+          class="button-card"
+          href="https://www.facebook.com/dylan.sheehan.5076/"
+          flat
+          round
+          size="0.75em"
+        >
+          <img
+            style="height: 3em"
+            src="../assets/facebook-button.png"
+            alt="Facebook Image"
+          />
+        </q-btn>
+      </q-card-actions>
     </q-card>
   </div>
 </template>
@@ -30,11 +85,20 @@ building on my Bachelor's from Texas Tech University. I have a passion for Full 
 and Cybersecurity, with a keen interest in Pretesting. Working as a Junior Software Engineer
 in the Department of Defense, I enjoy tackling complex challenges. In my free time, I love hiking,
 playing soccer, and golfing. Follow me elsewhere!`;
+const email: string = 'dylansheehan493@gmail.com';
+const phoneNum: string = '(708) 205-9618';
+
+function copyText(text: string) {
+  navigator.clipboard.writeText(text);
+}
 </script>
 
 <style lang="css">
 .business-card {
   width: 100%;
   max-width: 50em;
+}
+.button-card {
+  margin-right: 10px;
 }
 </style>
